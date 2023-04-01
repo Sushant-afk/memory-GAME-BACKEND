e80f025cd0a1ae5720fb860d58110b5e77e360b2
Sushant Yadav <56043218+Sushant-afk@users.noreply.github.com>
2023-04-01 15:11:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = process.env.MONGO_URI
 
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.use(express.json())
 
